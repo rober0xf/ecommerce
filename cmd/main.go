8a2795882862e6537f211ea97c93c065c6689a46
rober0xf <robertinopili13@gmail.com>
2024-12-23 23:32:15 -0300

    database config

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"ecommerce/config"
+	"ecommerce/db"
+	"ecommerce/internal/adapters/handler"
+	"log"
+)
+
+func main() {
+	config.LoadEnv()
+	dbConfig, err := db.NewDBConfig()
+	if err != nil {
+		log.Fatalf("error loading db config: %v", err)
+	}
+
+	db, err := db.InitDatabase(dbConfig)
+	if err != nil {
+		log.Fatalf("error initializing db: %v", err)
+	}
+	server := handler.NewAPIServer(":8000", db)
+	if err := server.Start(); err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
+}
---
+++ config/database.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"time"
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error: loading env file")
+	}
+}
+
+type DBConfig struct {
+	Host            string
+	User            string
+	Password        string
+	Name            string
+	Port            int
+	MaxConns        int
+	MaxIdleTime     time.Duration
+	MaxConnLifetime time.Duration
+}
+
+func (cfg *DBConfig) ConnString() string {
+	return fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
+}
