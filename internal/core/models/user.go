d82c4102fbdd4d0624e2be34934a14a90829a39e
rober0xf <robertinopili13@gmail.com>
2024-12-24 19:46:53 -0300

    validation json struct

--- internal/core/models/user.go
+++ internal/core/models/user.go
@@ -1,19 +1,28 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type User struct {
 	ID       int       `json:"id"`
 	Name     string    `json:"name"`
 	LastName string    `json:"lastname"`
 	Email    string    `json:"email"`
-	Password string    `json:"password"`
+	Password string    `json:"-"`
 	CreateAt time.Time `json:"created_at"`
 }
 
+// easier to test
+type UserStore interface {
+	GetUserByEmail(email string) (*User, error)
+	GetUserByID(id int) (*User, error)
+	CreateUser(user *User) error
+}
+
 type Payload struct {
-	Name     string `json:"name"`
-	LastName string `json:"lastname"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required"`
+	LastName string `json:"lastname" validate:"required"`
+	Email    string `json:"email" validate:"email,required"`
+	Password string `json:"password" validate:"required,min=5,max=50"`
 }
