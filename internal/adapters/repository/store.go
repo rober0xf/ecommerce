406e729fcaac29ae01cddc400f702a0a80b99abd
rober0xf <robertinopili13@gmail.com>
2024-12-27 15:09:06 -0300

    create user db

--- internal/adapters/repository/store.go
+++ internal/adapters/repository/store.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"ecommerce/internal/core/models"
+	"errors"
 	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,12 +19,12 @@ func NewStore(db *pgxpool.Pool) *Store {
 
 func (s *Store) GetUserByEmail(email string) (*models.User, error) {
 	var user models.User
-	query := `SELECT id, name, email, password FROM users WHERE email = $1`
+	query := `SELECT id, name, lastname, email, password, created_at FROM users WHERE email = $1`
 
 	err := s.DB.QueryRow(context.Background(), query, email).Scan(&user.ID, &user.Name, &user.LastName, &user.Email, &user.Password, &user.CreateAt)
 	if err != nil {
-		if err == pgx.ErrNoRows {
-			return nil, fmt.Errorf("user not found")
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, pgx.ErrNoRows
 		}
 		return nil, fmt.Errorf("failed to query user") // other error
 	}
@@ -32,9 +33,32 @@ func (s *Store) GetUserByEmail(email string) (*models.User, error) {
 }
 
 func (s *Store) GetUserByID(id int) (*models.User, error) {
-	return nil, nil
+	var user models.User
+	query := `SELECT id, name, lastname, email, password, created_at FROM users WHERE id = $1`
+
+	err := s.DB.QueryRow(context.Background(), query, id).Scan(&user.ID, &user.Name, &user.LastName, &user.Email, &user.Password, &user.CreateAt)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, fmt.Errorf("failed to query user") // other error
+	}
+
+	return &user, nil
 }
 
 func (s *Store) CreateUser(user *models.User) error {
+	query := `INSERT INTO users (name, lastname, email, password) VALUES ($1, $2, $3, $4) RETURNING id, created_at`
+	err := s.DB.QueryRow(context.Background(), query,
+		user.Name,
+		user.LastName,
+		user.Email,
+		user.Password,
+	).Scan(&user.ID, &user.CreateAt)
+
+	if err != nil {
+        return fmt.Errorf("error creating user: %w", err)
+	}
+
 	return nil
 }
