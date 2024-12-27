1fc1509dcdb3d61f09502a500c9185f0c901395d
rober0xf <robertinopili13@gmail.com>
2024-12-27 15:07:59 -0300

    login handler

--- internal/adapters/handler/routes.go
+++ internal/adapters/handler/routes.go
@@ -26,18 +26,14 @@ func (h *Handler) InitRoutes(router *mux.Router) {
 }
 
 func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	if r.Body == nil {
-		pkg.WriteError(w, fmt.Errorf("invalid request body"), http.StatusBadRequest)
+		pkg.WriteError(w, "invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	var payload models.Payload
+	var payload models.PayloadLogin
 	if err := pkg.ParseJSON(&payload, r); err != nil {
-		pkg.WriteError(w, fmt.Errorf("invalid request body"), http.StatusBadRequest)
+		pkg.WriteError(w, "invalid request body", http.StatusBadRequest)
 		return
 	}
 
@@ -45,21 +41,56 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	if err := ValidateStruct.Struct(payload); err != nil {
 		fmt.Printf("validation error: %v\n", err)
 		error_msg := err.(validator.ValidationErrors)
-		pkg.WriteError(w, fmt.Errorf("invalid input: %v", error_msg), http.StatusBadRequest)
+		pkg.WriteError(w, fmt.Sprintf("invalid input: %v", error_msg), http.StatusBadRequest)
+		return
+	}
+
+	// check if the user exists
+	user, err := h.Store.GetUserByEmail(payload.Email)
+	if err == nil {
+		pkg.WriteError(w, "user not found", http.StatusBadRequest)
+		return
+	}
+
+	if !services.CheckPassword(user.Password, payload.Password) {
+		pkg.WriteError(w, "invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	pkg.WriteJSON(w, http.StatusOK, nil)
+}
+
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		pkg.WriteError(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	var payload models.PayloadRegister
+	if err := pkg.ParseJSON(&payload, r); err != nil {
+		pkg.WriteError(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// validate json
+	if err := ValidateStruct.Struct(payload); err != nil {
+		fmt.Printf("validation error: %v\n", err)
+		error_msg := err.(validator.ValidationErrors)
+		pkg.WriteError(w, fmt.Sprintf("invalid input: %v", error_msg), http.StatusBadRequest)
 		return
 	}
 
 	// check if the user exists
 	_, err := h.Store.GetUserByEmail(payload.Email)
 	if err == nil {
-		pkg.WriteError(w, fmt.Errorf("user %s already exists", payload.Email), http.StatusBadRequest)
+		pkg.WriteError(w, fmt.Sprintf("user %s already exists", payload.Email), http.StatusBadRequest)
 		return
 	}
 
 	// hash password
 	hashed_password, err := services.HashPassword(payload.Password)
 	if err != nil {
-		pkg.WriteError(w, fmt.Errorf("invalid password"), http.StatusInternalServerError)
+		pkg.WriteError(w, "password error", http.StatusInternalServerError)
 		return
 	}
 
@@ -72,7 +103,7 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	})
 
 	if err != nil {
-		pkg.WriteError(w, fmt.Errorf("error creating user"), http.StatusInternalServerError)
+		pkg.WriteError(w, "error creating user", http.StatusInternalServerError)
 		return
 	}
 
